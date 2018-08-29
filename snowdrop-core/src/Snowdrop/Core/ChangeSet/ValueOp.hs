3384318b7273eeafa42455658ea605df701c414e
Ilya Peresadin <pva701@gmail.com>
2018-08-29 21:31:24 +0300

    [SD-142] Document Snowdrop.Core.ChangeSet module

--- snowdrop-core/src/Snowdrop/Core/ChangeSet/ValueOp.hs
+++ snowdrop-core/src/Snowdrop/Core/ChangeSet/ValueOp.hs
@@ -2,6 +2,8 @@
 {-# LANGUAGE DeriveFunctor     #-}
 {-# LANGUAGE DeriveTraversable #-}
 
+-- | ValueOp primitive.
+
 module Snowdrop.Core.ChangeSet.ValueOp
        (
          ValueOp (..)
@@ -12,11 +14,30 @@ import           Universum hiding (head, init, last)
 
 import           Snowdrop.Util
 
+-- | ValueOp datatype is an action which should be performed over an entry
+-- stored in a key-value storage. Intended to be used in @ChangeSet@,
+-- in Map Id (ValueOp Value).
 data ValueOp v
     = New v
+    -- ^ @New@ operation requires the following invariants (both of them):
+    --   1. key which holds this ValueOp doesn't exist in a state,
+    --   2. after aplication of entry (key, New v) to the state a value stored
+    --      by @key@ is @v@.
     | Upd v
+    -- ^ @Upd@ operation requires the following invariants (both of them):
+    --   1. key which holds this ValueOp exists in a state,
+    --   2. after aplication of entry (key, Upd v) to the state a value stored
+    --      by @key@ is @v@.
     | Rem
+    -- ^ @Rem@ operation requires the following invariants (both of them):
+    --   1. key which holds this ValueOp exists in a state,
+    --   2. after aplication of entry (key, Rem) to the state the @key@ doesn't
+    -- exist in the state anymore.
     | NotExisted
+    -- ^ @NotExisted@ operation requires the following invariant:
+    -- key which holds this ValueOp doesn't exist in a state.
+    -- This operation has no side effects with respect to the state.
+    -- However, if invariant isn't hold an exception will be caused.
     deriving (Functor, Foldable, Traversable, Show, Eq, Ord, Generic)
 
 instance HasReview v v1 => HasReview (ValueOp v) (ValueOp v1) where
@@ -28,6 +49,9 @@ instance HasPrism v v1 => HasPrism (ValueOp v) (ValueOp v1) where
     proj (New v)    = New <$> proj v
     proj (Upd v)    = Upd <$> proj v
 
+-- | Auxiliary datatype to provide Semigroup instance for ValueOp.
+-- ValueOp doesn't hold Semigroup laws, but it holds if there is
+-- an additional constructor corresponding error during mappend.
 data ValueOpEx v
     = Op (ValueOp v)
     | Err
