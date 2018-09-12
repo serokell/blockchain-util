c7cc70d41a833d046a2bcec8244709c5d82db967
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-09-12 11:23:57 +0000

    Repin stack4nix

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@
 let
   stack4nix = fetchGit {
     url = https://github.com/serokell/stack4nix;
-    rev = "26a46991270749ecc57d5842d391435b314dc26f";
+    rev = "dee5f58317a0f067c6adfceed4d710e53d56cac5";
   };
 
   buildStackProject = import stack4nix { inherit pkgs; };
